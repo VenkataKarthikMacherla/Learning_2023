3a6668c3a5eb459a649c01bbd900f49094c912af
VenkataKarthikMacherla <karthikmacherla7@gmail.com>
2023-06-22 12:47:33 +0530

    Initial Commit

--- Day 3/Day3Q5.c
+++ Day 3/Day3Q5.c
@@ -1,46 +1,50 @@
 #include <stdio.h>
 
-void findSmallestLargestDigits(int n, int numbers[]) {
-    int i, smallest, largest;
-    smallest = 9; // Initialize smallest with the largest possible digit
-    largest = 0;  // Initialize largest with the smallest possible digit
+void findMinMaxDigits(int n, int numbers[]) {
+    int i, j;
+    int smallestDigit = 9;
+    int largestDigit = 0;
 
-    // Iterate through each number
     for (i = 0; i < n; i++) {
-        int temp = numbers[i];
-        
-        // Find the smallest and largest digits in the current number
-        while (temp > 0) {
-            int digit = temp % 10;
-            if (digit < smallest)
-                smallest = digit;
-            if (digit > largest)
-                largest = digit;
-            temp /= 10;
+        int num = numbers[i];
+
+        while (num != 0) {
+            int digit = num % 10;
+
+            if (digit < smallestDigit) {
+                smallestDigit = digit;
+            }
+
+            if (digit > largestDigit) {
+                largestDigit = digit;
+            }
+
+            num /= 10;
         }
     }
 
-    // Check if the input numbers are valid (i.e., at least one digit found)
-    if (smallest == 9 && largest == 0) {
+    if (smallestDigit == 9 || largestDigit == 0) {
         printf("Not Valid\n");
     } else {
-        printf("Smallest digit: %d\n", smallest);
-        printf("Largest digit: %d\n", largest);
+        printf("Smallest Digit: %d\n", smallestDigit);
+        printf("Largest Digit: %d\n", largestDigit);
     }
 }
 
 int main() {
     int n, i;
+
     printf("Enter the value of n: ");
     scanf("%d", &n);
 
     int numbers[n];
-    printf("Enter %d numbers: ", n);
+
     for (i = 0; i < n; i++) {
+        printf("Enter number %d: ", i + 1);
         scanf("%d", &numbers[i]);
     }
 
-    findSmallestLargestDigits(n, numbers);
+    findMinMaxDigits(n, numbers);
 
     return 0;
 }
