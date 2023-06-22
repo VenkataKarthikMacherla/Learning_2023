c5c1745746dd6e2c16837d22976abb397955e4cd
VenkataKarthikMacherla <karthikmacherla7@gmail.com>
2023-06-22 12:46:29 +0530

    Initial Commit

--- Day 3/Day3Q4.c
+++ Day 3/Day3Q4.c
@@ -1,32 +1,25 @@
 #include <stdio.h>
 
-int findLargestNumber(int number) {
+int deleteDigit(int num, int position) {
+    int pow = 1;
+    int temp = num;
+
+    while (temp / 10 != 0) {
+        pow *= 10;
+        temp /= 10;
+    }
+
+    return (num / (pow * 10)) * pow + (num % pow);
+}
+
+int findLargestNumber(int num) {
     int largest = 0;
-    int divisor = 1;
-    int temp = number;
 
-    // Iterate through each digit position
     for (int i = 0; i < 4; i++) {
-        temp = number;
-        int deletedNumber = 0;
-
-        // Delete the digit at the current position
-        for (int j = 0; j < 4; j++) {
-            int digit = temp % 10;
-            temp /= 10;
-            
-            if (j != i) {
-                deletedNumber += digit * divisor;
-                divisor *= 10;
-            }
+        int deletedNum = deleteDigit(num, i);
+        if (deletedNum > largest) {
+            largest = deletedNum;
         }
-
-        // Check if the deleted number is larger than the current largest
-        if (deletedNumber > largest) {
-            largest = deletedNumber;
-        }
-
-        divisor = 1;
     }
 
     return largest;
@@ -34,11 +27,13 @@ int findLargestNumber(int number) {
 
 int main() {
     int number;
+
     printf("Enter a 4-digit number: ");
     scanf("%d", &number);
 
     int largestNumber = findLargestNumber(number);
-    printf("The largest number by deleting a single digit: %d\n", largestNumber);
+
+    printf("The largest number after deleting a single digit: %d\n", largestNumber);
 
     return 0;
 }
